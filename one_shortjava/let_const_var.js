9c74e5aec4cbb9df5b52fa702a07a9141fa76bf7
Sahil Singh <sahil@example.com>
2025-03-08 12:42:07 +0530

    Updated files with new changes

--- one_shortjava/let_const_var.js
+++ one_shortjava/let_const_var.js
@@ -1,1 +1,17 @@
-console.log("sfsf");
+const accountId=1234
+let accountEmail ="Sahil@gmail.com";
+var accountPassword ="5858";
+accountCity = "Patial";
+
+// accountId= 2
+ accountEmail ="opop@gmail.com"
+ accountPassword ="000"
+accountCity = "l"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
